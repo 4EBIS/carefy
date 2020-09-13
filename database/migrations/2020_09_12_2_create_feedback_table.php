6e73c8a34abac07a7ec8d303a2610a036f952787
Raí Gonçalves <rg.silva@outlook.com>
2020-09-13 01:58:06 -0300

    ajuste de relacionamento com a tabela 'comments'

--- database/migrations/2020_09_12_2_create_feedback_table.php
+++ database/migrations/2020_09_12_2_create_feedback_table.php
@@ -16,8 +16,10 @@ class CreateFeedbackTable extends Migration
         Schema::create('feedbacks', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('user_id');
-            $table->string('vote_id');
+            $table->integer('vote_id');
+            $table->integer('comment_id');
             $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('comment_id')->references('id')->on('comments');
             $table->foreign('vote_id')->references('id')->on('votes');
             $table->timestamp('created_at')->nullable();
         });
